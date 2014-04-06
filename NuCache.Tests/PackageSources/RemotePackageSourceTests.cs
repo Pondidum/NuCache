92fa4b71facddae2fca8abf774f9ff7475d855fe
andy <andy@andydote.co.uk>
2014-04-06 22:44:05 +0100

    remotePackageSourceTests: add tests for all methods

--- NuCache.Tests/PackageSources/RemotePackageSourceTests.cs
+++ NuCache.Tests/PackageSources/RemotePackageSourceTests.cs
@@ -7,16 +7,47 @@ namespace NuCache.Tests.PackageSources
 {
 	public class RemotePackageSourceTests
 	{
-		public void When_calling_get()
+
+		private void Test(Action<RemotePackageSource, Uri> method)
 		{
 			var client = Substitute.For<WebClient>();
 			var transformer = new UriHostTransformer(new Uri("http://localhost.fiddler:42174"));
 
 			var source = new RemotePackageSource(client, transformer);
 
-			source.Get(new Uri("http://example.com/api/v2"));
+			method(source, new Uri("http://example.com/api/v2"));
 
-			client.Received().GetResponseAsync(new Uri("http://localhost.fiddler:42174/api/v2"));
+			client.Received().GetResponseAsync(new Uri("http://localhost.fiddler:42174/api/v2"));	
+		}
+
+		public void When_calling_get()
+		{
+			Test((s,u) => s.Get(u));
+		}
+
+		public void When_calling_metadata()
+		{
+			Test((s,u) => s.Metadata(u));
+		}
+
+		public void When_calling_list()
+		{
+			Test((s,u) => s.List(u));
+		}
+
+		public void When_calling_search()
+		{
+			Test((s,u) => s.Search(u));
+		}
+
+		public void When_calling_findPackagesByID()
+		{
+			Test((s,u) => s.FindPackagesByID(u));
+		}
+
+		public void When_calling_getPackageByID()
+		{
+			Test((s, u) => s.GetPackageByID(u));
 		}
 	}
 }
