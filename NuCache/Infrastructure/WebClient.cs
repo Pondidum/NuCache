5f305c6bc30f3a96c1034c1443613454f4403883
andy <andy@andydote.co.uk>
2014-04-06 14:21:19 +0100

    webClient: removed unused method

--- NuCache/Infrastructure/WebClient.cs
+++ NuCache/Infrastructure/WebClient.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.IO;
-using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 
@@ -8,19 +6,6 @@ namespace NuCache.Infrastructure
 {
 	public class WebClient
 	{
-		public string MakeRequest(Uri url)
-		{
-			var request = (HttpWebRequest) WebRequest.Create(url);
-			request.ContentType = "text/xml";
-
-			using (var response = request.GetResponse())
-			using (var stream  = response.GetResponseStream())
-			using (var reader = new StreamReader(stream))
-			{
-				return reader.ReadToEnd();
-			}
-		}
-
 		public async Task<HttpResponseMessage> GetResponseAsync(Uri url)
 		{
 			var client = new HttpClient();
