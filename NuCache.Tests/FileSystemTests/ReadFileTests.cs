f49ccbf95e7b613d53068d6b319279fc4538d7f2
andy <andy@andydote.co.uk>
2014-04-07 15:09:13 +0100

    fileSystemTests: add readFile tests

---
+++ NuCache.Tests/FileSystemTests/ReadFileTests.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using Should;
+using Should.Core.Assertions;
+
+namespace NuCache.Tests.FileSystemTests
+{
+	public class ReadFileTests : FileSystemTestBase
+	{
+		private const string Contents = "Some test string with !£$%^&*() characters.";
+
+		private string StringFromStream(Stream stream)
+		{
+			using (var sr = new StreamReader(stream))
+			{
+				return sr.ReadToEnd();
+			}
+		}
+
+		public void When_reading_from_an_existing_file()
+		{
+			File.WriteAllText(Filename, Contents);
+
+			StringFromStream(FileSystem.ReadFile(Filename)).ShouldEqual(Contents);
+		}
+
+		public void When_reading_from_a_non_existing_file()
+		{
+			File.Delete(Filename);
+
+			Assert.Throws<FileNotFoundException>(() => FileSystem.ReadFile(Filename));
+		}
+	}
+}
