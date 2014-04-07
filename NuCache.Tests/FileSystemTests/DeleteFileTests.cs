ea46ff76464d0c95802ec648dfb866be6a86434c
andy <andy@andydote.co.uk>
2014-04-07 15:18:18 +0100

    fileSystemTests: add DeleteFileTests

---
+++ NuCache.Tests/FileSystemTests/DeleteFileTests.cs
@@ -0,0 +1,41 @@
+﻿using System.IO;
+using Should;
+
+namespace NuCache.Tests.FileSystemTests
+{
+	public class DeleteFileTests : FileSystemTestBase
+	{
+		public void When_deleting_an_existing_file_by_relative_path()
+		{
+			FileSystem.DeleteFile(Filename);
+
+			File.Exists(Filename).ShouldBeFalse();
+		}
+
+		public void When_deleting_an_existing_file_by_absolute_path()
+		{
+			var absolute = Path.GetFullPath(Filename);
+			FileSystem.DeleteFile(absolute);
+
+			File.Exists(Filename).ShouldBeFalse();
+		}
+
+		public void When_deleting_a_non_existing_file_by_relative_path()
+		{
+			File.Delete(Filename);
+			FileSystem.DeleteFile(Filename);
+
+			File.Exists(Filename).ShouldBeFalse();
+		}
+
+		public void When_deleting_a_non_existing_file_by_absolute_path()
+		{
+			File.Delete(Filename);
+
+			var absolute = Path.GetFullPath(Filename);
+			FileSystem.DeleteFile(absolute);
+
+			File.Exists(Filename).ShouldBeFalse();
+		}
+	}
+}
