0d87c609ce49c3fb2e47d7da0788dbf6116ce180
andy <andy@andydote.co.uk>
2014-04-07 15:27:20 +0100

    fileSystemTests: add base class for directory tests

---
+++ NuCache.Tests/FileSystemTests/BaseFileSystemDirectoryTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace NuCache.Tests.FileSystemTests
+{
+	public class BaseFileSystemDirectoryTest : IDisposable
+	{
+		protected string DirectoryName;
+		protected FileSystem FileSystem;
+
+		public BaseFileSystemDirectoryTest()
+		{
+			FileSystem = new FileSystem();
+
+			DirectoryName = Guid.NewGuid().ToString();
+
+			Directory.CreateDirectory(DirectoryName);
+		}
+
+		public void Dispose()
+		{
+			try
+			{
+				if (Directory.Exists(DirectoryName))
+				{
+					Directory.Delete(DirectoryName);
+				}
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine("Enable to delete '{0}'", DirectoryName);
+			}
+
+		}
+	}
+}
