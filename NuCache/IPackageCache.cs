bc5ae024f54d5c598cc5ec7a526d23731548abf5
andy <andy@andydote.co.uk>
2014-04-07 09:34:19 +0100

    nucache: add IPackageCache

---
+++ NuCache/IPackageCache.cs
@@ -0,0 +1,12 @@
+﻿using System.IO;
+
+namespace NuCache
+{
+	public interface IPackageCache
+	{
+		bool Contains(string name, string version);
+		void Write(string name, string version, Stream contents);
+		void Remove(string name, string version);
+		void RemoveAll();
+	}
+}
