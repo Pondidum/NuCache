f67b579762ac872027b260dbe8eeb521fceeca54
andy <andy@andydote.co.uk>
2014-04-07 11:51:48 +0100

    fileSystem: add implementation of IFileSystem, start on tests

---
+++ NuCache/FileSystem.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace NuCache
+{
+	public class FileSystem : IFileSystem
+	{
+		public bool FileExists(string path)
+		{
+			return File.Exists(path);
+		}
+
+		public void WriteFile(string path, Stream contents)
+		{
+			using (var fs = new FileStream(path, FileMode.Create, FileAccess.Write))
+			{
+				contents.CopyTo(fs);
+			}
+		}
+
+		public Stream ReadFile(string path)
+		{
+			return new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.Read);
+		}
+
+		public void DeleteFile(string path)
+		{
+			File.Delete(path);
+		}
+
+		public bool DirectoryExists(string path)
+		{
+			return Directory.Exists(path);
+		}
+
+		public void CreateDirectory(string path)
+		{
+			Directory.CreateDirectory(path);
+		}
+
+		public IEnumerable<string> ListDirectory(string path)
+		{
+			return Directory.GetFileSystemEntries(path);
+		}
+
+		public void DeleteDirectory(string path)
+		{
+			Directory.Delete(path);
+		}
+	}
+}
