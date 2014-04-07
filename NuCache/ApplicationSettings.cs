311ca7b3180cbeba910ed34e156bc164688fd890
andy <andy@andydote.co.uk>
2014-04-07 09:57:09 +0100

    applicationSettings: add settings class

---
+++ NuCache/ApplicationSettings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Configuration;
+
+namespace NuCache
+{
+	public class ApplicationSettings
+	{
+		public virtual string CachePath
+		{
+			get { return ConfigurationManager.AppSettings["CachePath"]; }
+		}
+
+		public virtual Uri RemoteFeed
+		{
+			get { return new Uri(ConfigurationManager.AppSettings["RemoteFeed"]); }
+		}
+	}
+}
