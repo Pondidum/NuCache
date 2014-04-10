9305b435eaf1fd63f462f7c436362f86893aa64c
andy <andy@andydote.co.uk>
2014-04-10 11:53:54 +0100

    tests: added proxyBehaviourSetTests

---
+++ NuCache.Tests/ProxyBehaviour/ProxyBehaviourSetTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+using NSubstitute;
+using NuCache.ProxyBehaviour;
+using Should.Core.Assertions;
+
+namespace NuCache.Tests.ProxyBehaviour
+{
+	public class ProxyBehaviourSetTests
+	{
+
+		public void When_there_are_no_behaviours()
+		{
+			var set = new ProxyBehaviourSet(Enumerable.Empty<IProxyBehaviour>());
+
+			Assert.DoesNotThrow(() => set.Execute(new Uri("http://example.com"), new HttpResponseMessage()));
+		}
+
+		public void When_there_are_behaviours()
+		{
+			var b1 = Substitute.For<IProxyBehaviour>();
+			var b2 = Substitute.For<IProxyBehaviour>();
+
+			var set = new ProxyBehaviourSet(new[] { b1, b2 });
+
+			var message = Substitute.For<HttpResponseMessage>();
+			var url = new Uri("http://example.com");
+
+			set.Execute(url, message);
+
+			b1.Received().Execute(url, message);
+			b2.Received().Execute(url, message);
+		}
+
+	}
+}
