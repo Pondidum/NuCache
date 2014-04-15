6e709ed7cc732c9666c0e9606fe78d046c193c6b
andy <andy@andydote.co.uk>
2014-04-15 13:01:23 +0100

    nuget: add classes for parsing .nupkg contents

---
+++ NuCache/Infrastructure/NuGet/Manifest.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using System.Xml.Linq;
+
+namespace NuCache.Infrastructure.NuGet
+{
+	public class Manifest
+	{
+		public string Name { get; private set; }
+		public string Version { get; private set; }
+		public PackageID ID { get; private set; }
+
+		public Manifest(Stream stream)
+		{
+			var doc = XDocument.Load(stream);
+			var ns = doc.Root.Name.Namespace;
+
+			var manifest = doc.Root.Element(ns + "metadata");
+
+			Name = manifest.Element(ns + "id").Value;
+			Version = manifest.Element(ns + "version").Value;
+			ID = new PackageID(Name, Version);
+
+			//dont care about the rest for now
+		}
+	}
+}
---
+++ NuCache/Infrastructure/NuGet/Package.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using System.Linq;
+using Packaging = System.IO.Packaging.Package;
+
+namespace NuCache.Infrastructure.NuGet
+{
+	public class Package
+	{
+		internal const string PackageRelationshipNamespace = "http://schemas.microsoft.com/packaging/2010/07/";
+		internal const string ManifestRelationType = "manifest";
+
+		public Manifest Metadata { get; set; }
+
+		public Package(Stream stream)
+		{
+
+			var package = Packaging.Open(stream);
+
+			var relationshipType = package.GetRelationshipsByType(PackageRelationshipNamespace + ManifestRelationType).SingleOrDefault();
+			var manifestPart = package.GetPart(relationshipType.TargetUri);
+
+			using (var manifestStream = manifestPart.GetStream())
+			{
+				Metadata = new Manifest(manifestStream);
+			}
+		}
+	}
+}
