1e1a13afa5183c128b0633d3139c76b7984ac3bd
andy <andy@andydote.co.uk>
2014-04-24 13:51:41 +0100

    manage: start on cache management page

---
+++ NuCache/Controllers/ManageController.cs
@@ -0,0 +1,23 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using NuCache.Infrastructure.Spark;
+using NuCache.Models;
+
+namespace NuCache.Controllers
+{
+	public class ManageController : ApiController
+	{
+		private readonly SparkResponseFactory _responseFactory;
+
+		public ManageController(SparkResponseFactory responseFactory)
+		{
+			_responseFactory = responseFactory;
+		}
+
+		public HttpResponseMessage GetIndex()
+		{
+			return _responseFactory.From(new ManageViewModel());
+		}
+	}
+}
