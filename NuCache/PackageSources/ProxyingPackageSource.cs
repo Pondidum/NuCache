2b57e9e35009316c96ce86780f469b73bf0be87d
andy <andy@andydote.co.uk>
2014-05-08 00:09:07 +0100

    proxyingPackageSource: clean up using statements

--- NuCache/PackageSources/ProxyingPackageSource.cs
+++ NuCache/PackageSources/ProxyingPackageSource.cs
@@ -1,11 +1,9 @@
 ﻿using System;
-using System.Net;
 using System.Net.Http;
-using System.Net.Http.Headers;
 using System.Threading.Tasks;
+using NuCache.Infrastructure;
 using NuCache.ProxyBehaviour;
 using NuCache.Rewriters;
-using WebClient = NuCache.Infrastructure.WebClient;
 
 namespace NuCache.PackageSources
 {
