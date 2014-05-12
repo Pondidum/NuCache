50f6a33650140470af5046765baeaffebf9bff58
andy <andy@andydote.co.uk>
2014-05-12 22:34:37 +0100

    statisticsController: stub out return value, add to main navigation

--- NuCache/Controllers/StatisticsController.cs
+++ NuCache/Controllers/StatisticsController.cs
@@ -1,9 +1,22 @@
-﻿using System.Web.Http;
+﻿using System.Net.Http;
+using System.Web.Http;
+using NuCache.Infrastructure.Spark;
+using NuCache.Models;
 
 namespace NuCache.Controllers
 {
 	public class StatisticsController : ApiController
 	{
-		
+		private readonly SparkResponseFactory _responseFactory;
+
+		public StatisticsController(SparkResponseFactory responseFactory)
+		{
+			_responseFactory = responseFactory;
+		}
+
+		public HttpResponseMessage GetIndex()
+		{
+			return _responseFactory.From(new StatisticsViewModel());
+		}
 	}
 }
