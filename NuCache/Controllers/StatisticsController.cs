8b44d91e70102fc68bc2375acc9371ea1d49aba4
andy <andy@andydote.co.uk>
2014-05-18 22:37:57 +0100

    statistics: add content to stats page

--- NuCache/Controllers/StatisticsController.cs
+++ NuCache/Controllers/StatisticsController.cs
@@ -1,6 +1,8 @@
 ﻿using System.Net.Http;
 using System.Web.Http;
 using NuCache.Infrastructure.Spark;
+using NuCache.Infrastructure.Statistics;
+using NuCache.Infrastructure.Statistics.Processors;
 using NuCache.Models;
 
 namespace NuCache.Controllers
@@ -8,15 +10,24 @@ namespace NuCache.Controllers
 	public class StatisticsController : ApiController
 	{
 		private readonly SparkResponseFactory _responseFactory;
+		private readonly StatisticsCollector _statistics;
 
-		public StatisticsController(SparkResponseFactory responseFactory)
+		public StatisticsController(SparkResponseFactory responseFactory, StatisticsCollector statistics)
 		{
 			_responseFactory = responseFactory;
+			_statistics = statistics;
 		}
 
 		public HttpResponseMessage GetIndex()
 		{
-			return _responseFactory.From(new StatisticsViewModel());
+			var downloads = _statistics.GetStatistic<PackageDownloads>();
+
+			var model = new StatisticsViewModel
+			{
+				DownloadCounts = downloads.PackageCounts
+			};
+
+			return _responseFactory.From(model);
 		}
 	}
 }
--- NuCache/Models/StatisticsViewModel.cs
+++ NuCache/Models/StatisticsViewModel.cs
@@ -1,7 +1,10 @@
-﻿namespace NuCache.Models
+﻿using System.Collections.Generic;
+using NuCache.Infrastructure.NuGet;
+
+namespace NuCache.Models
 {
 	public class StatisticsViewModel
 	{
-		 
+		public IEnumerable<KeyValuePair<PackageID, int>> DownloadCounts { get; set; }
 	}
 }
