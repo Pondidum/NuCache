b02db7c3d1d8d76dad96154cc9f9caf7ade955bd
andy <andy@andydote.co.uk>
2014-05-18 22:37:03 +0100

    statistics: add a statisticsRegistry

---
+++ NuCache/Infrastructure/Statistics/StatisticsRegistry.cs
@@ -0,0 +1,20 @@
+﻿using StructureMap.Configuration.DSL;
+using StructureMap.Graph;
+
+namespace NuCache.Infrastructure.Statistics
+{
+	public class StatisticsRegistry : Registry
+	{
+		public StatisticsRegistry()
+		{
+			Scan(s =>
+			{
+				s.TheCallingAssembly();
+				s.AddAllTypesOf<IStatistic<HttpStatistic>>();
+			});
+
+			
+			
+		}
+	}
+}
