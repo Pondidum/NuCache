525b7de816ead1921f09a1985b527fa0a2483554
andy <andy@andydote.co.uk>
2016-03-13 14:48:04 +0000

    startup: implement simple proxying

--- NuCache.Tests/Scratchpad.cs
+++ NuCache.Tests/Scratchpad.cs
@@ -1,4 +1,12 @@
-﻿using Xunit;
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Microsoft.Owin.Testing;
+using NuCache.Properties;
+using Owin;
+using Shouldly;
+using Xunit;
 using Xunit.Abstractions;
 
 namespace NuCache.Tests
@@ -13,8 +21,29 @@ namespace NuCache.Tests
 		}
 
 		[Fact]
-		public void When_testing_something()
+		public async void When_testing_something()
 		{
+			Func<IOwinContext, Func<Task>, Task> appHandler = (context, next) =>
+			{
+				_output.WriteLine(context.Request.Uri.ToString());
+				context.Response.StatusCode = (int)HttpStatusCode.OK;
+				context.Response.ContentType = "text/plain";
+				return context.Response.WriteAsync(context.Request.Path.ToString());
+			};
+
+			var api = new Startup();
+			var host = TestServer.Create(api.Configuration);
+
+
+			//https://api.nuget.org/
+			var result = await host.CreateRequest("v3/registration1/finite/index.json").GetAsync();
+			//var result = await host.CreateRequest("v3/index.json").GetAsync();
+
+			result.IsSuccessStatusCode.ShouldBe(true);
+
+			_output.WriteLine(await result.Content.ReadAsStringAsync());
+
+			host.Dispose();
 		} 
 	}
 }
--- NuCache/Properties/Startup.cs
+++ NuCache/Properties/Startup.cs
@@ -1,6 +1,10 @@
-﻿using Microsoft.Owin;
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using Microsoft.Owin;
 using NuCache.Properties;
 using Owin;
+using Owin.Routing;
 
 [assembly:OwinStartup(typeof(Startup))]
 
@@ -10,10 +14,34 @@ namespace NuCache.Properties
 	{
 		public void Configuration(IAppBuilder app)
 		{
-			app.Run(context =>
+			var rootUrl = new Uri("http://api.nuget.org");
+
+			app.Use(async (context, next) =>
 			{
-				context.Response.ContentType = "text/plain";
-				return context.Response.WriteAsync("OI oi");
+				if (context.Request.Path.ToString() != "/v3/index.json")
+					await next();
+				else
+					await context.Response.WriteAsync("[]");
+			});
+
+			app.Use(async (context, next) =>
+			{
+				var client = new HttpClient()
+				{
+					BaseAddress = rootUrl
+				};
+
+				var response = await client.GetAsync(context.Request.Path.ToString());
+
+				using (var sr = new StreamReader(await response.Content.ReadAsStreamAsync()))
+				using (var sw = new StreamWriter(context.Response.Body))
+				{
+					string line;
+					while ((line = sr.ReadLine()) != null)
+					{
+						sw.WriteLine(line);
+					}
+				}
 			});
 		} 
 	}
