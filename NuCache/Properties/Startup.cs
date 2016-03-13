82cf20d06f52dc66e8d2b8a2b89ac279c5963dca
andy <andy@andydote.co.uk>
2016-03-13 13:47:08 +0000

    init: basic owin based webapp

---
+++ NuCache/Properties/Startup.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Owin;
+using NuCache.Properties;
+using Owin;
+
+[assembly:OwinStartup(typeof(Startup))]
+
+namespace NuCache.Properties
+{
+	public class Startup
+	{
+		public void Configuration(IAppBuilder app)
+		{
+			app.Run(context =>
+			{
+				context.Response.ContentType = "text/plain";
+				return context.Response.WriteAsync("OI oi");
+			});
+		} 
+	}
+}
