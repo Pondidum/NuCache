a86e8a715495334c2731fb237d9d70ce97311cc4
andy <andy@andydote.co.uk>
2016-03-26 09:26:23 +0000

    client: basic layout

--- NuCache/client/components/app.js
+++ NuCache/client/components/app.js
@@ -1,7 +1,38 @@
 import React from 'react'
 
+const PackageTile = () => (
+  <div className="list-group col-xs-4">
+    <div className="list-group-item">
+      <div className="row-action-primary">
+        <i className="material-icons">folder</i>
+      </div>
+      <div className="row-content">
+        <div className="least-content">20</div>
+        <h4 className="list-group-item-heading">Package Name Here</h4>
+        <p className="list-group-item-text">Package description perhaps could go here? Or date added?</p>
+      </div>
+    </div>
+  </div>
+);
+
+const PackageList = () => (
+  <div className="row">
+    <PackageTile />
+    <PackageTile />
+    <PackageTile />
+    <PackageTile />
+    <PackageTile />
+    <PackageTile />
+    <PackageTile />
+    <PackageTile />
+  </div>
+);
+
 const App = () => (
-  <h1>NuCache</h1>
+  <div>
+    <h1>NuCache</h1>
+    <PackageList />
+  </div>
 );
 
 export default App
