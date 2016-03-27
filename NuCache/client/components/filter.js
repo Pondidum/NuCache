5dc031b1904f21276108c3e9db020167c3d7d3f3
andy <andy@andydote.co.uk>
2016-03-27 11:28:26 +0100

    client: trigger onChange when filter changes

--- NuCache/client/components/filter.js
+++ NuCache/client/components/filter.js
@@ -1,12 +1,23 @@
 import React from 'react'
 import { Input } from 'react-bootstrap'
 
-const Filter = () => (
-  <div className="row" style={{ marginBottom: "1em" }}>
-    <div className="col-md-4 col-md-offset-8">
-      <Input type="text" placeholder="filter..." standalone />
+const Filter = ({ onChange }) => {
+
+  var input;
+
+  return (
+    <div className="row" style={{ marginBottom: "1em" }}>
+      <div className="col-md-4 col-md-offset-8">
+        <Input
+          type="text"
+          placeholder="filter..."
+          ref={component => input = component}
+          onChange={() => onChange(input.getValue()) } 
+          standalone
+        />
+      </div>
     </div>
-  </div>
-);
+  );
+}
 
 export default Filter;
