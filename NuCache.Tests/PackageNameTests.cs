def0833e2a504318786f0e6402697fb447f4e516
andy <andy@andydote.co.uk>
2016-03-28 11:25:16 +0100

    packageName: add class for parsing package strings, with tests

---
+++ NuCache.Tests/PackageNameTests.cs
@@ -0,0 +1,38 @@
+﻿using Shouldly;
+using Xunit;
+
+namespace NuCache.Tests
+{
+	public class PackageNameTests
+	{
+		[Fact]
+		public void When_parsing_a_semver_filename_string()
+		{
+			var package = PackageName.Parse("finite.4.1.0.nupkg");
+
+			package.Name.ShouldBe("finite");
+			package.Version.ShouldBe("4.1.0");
+			package.ToString().ShouldBe("finite.4.1.0.nupkg");
+		}
+
+		[Fact]
+		public void When_parsing_a_dotnet_filename_string()
+		{
+			var package = PackageName.Parse("finite.4.1.0.56.nupkg");
+
+			package.Name.ShouldBe("finite");
+			package.Version.ShouldBe("4.1.0.56");
+			package.ToString().ShouldBe("finite.4.1.0.56.nupkg");
+		}
+
+		[Fact]
+		public void When_loading_from_name_and_version()
+		{
+			var package = new PackageName("finite", "4.1.0");
+
+			package.Name.ShouldBe("finite");
+			package.Version.ShouldBe("4.1.0");
+			package.ToString().ShouldBe("finite.4.1.0.nupkg");
+		}
+	}
+}
---
+++ NuCache/PackageName.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.RegularExpressions;
+
+namespace NuCache
+{
+	public class PackageName
+	{
+		public string Name { get; }
+		public string Version { get; }
+
+		public static PackageName Parse(string fileName)
+		{
+			var expression = new Regex(@"^(?<name>.*?)\.(?<version>(\d*\.){2,3}\d*)\.");
+			var match = expression.Match(fileName);
+
+			var name = match.Groups["name"].Value;
+			var version = match.Groups["version"].Value;
+
+			return new PackageName(name, version);
+		}
+
+		public PackageName(string name, string version)
+		{
+			Name = name.Trim();
+			Version = version.Trim();
+		}
+
+		public override string ToString()
+		{
+			return $"{Name}.{Version}.nupkg";
+		}
+	}
+}
