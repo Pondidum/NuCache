e965eab85a2c901cb5c578f73a8b0a2ad1c17c29
andy <andy@andydote.co.uk>
2016-03-29 23:19:26 +0100

    client: add interfaceMiddleware to host simple json api

---
+++ NuCache/Middlewares/InterfaceMiddleware.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Owin;
+using Microsoft.Owin.StaticFiles;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using Owin;
+using Owin.Routing;
+
+namespace NuCache.Middlewares
+{
+	public class InterfaceMiddleware
+	{
+		private readonly Statistics _stats;
+		private readonly JsonSerializerSettings _settings;
+
+		public InterfaceMiddleware(Statistics stats)
+		{
+			_stats = stats;
+			_settings = new JsonSerializerSettings
+			{
+				Formatting = Formatting.Indented,
+				ContractResolver = new CamelCasePropertyNamesContractResolver()
+			};
+		}
+
+		public void Configuration(IAppBuilder app)
+		{
+			app.Get("/api/stats", async context =>
+			{
+				await context.WriteJson(_stats.ForAll(), _settings);
+			});
+		}
+	}
+}
